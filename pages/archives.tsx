5f9df554282ff64ff7feeadf66706ceef4a80365
kasamina80 <kasamina180@gmail.com>
2024-11-04 16:38:35 +0900

    成果物に分数ビジュアライザを追加

--- pages/archives.tsx
+++ pages/archives.tsx
@@ -13,6 +13,8 @@ const ProfilePage = () => {
           <dd>左に出ている数字です。</dd>
           <dt><Link href="/bbs">簡易BBS</Link></dt>
           <dd>キリ番をgetした時に書き込む場所です。踏み逃げ禁止！</dd>
+          <dt><Link href="https://fraction-visualizer.vercel.app/">分数ビジュアライザ</Link></dt>
+          <dd>小学生向け: 分数の大きさを直感的に理解するためのサイトです。<br />※注意: 音が出ます</dd>
         </dl>
       </div>
     </div>
